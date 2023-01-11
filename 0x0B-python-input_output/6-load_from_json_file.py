573366cbdf739000a110228baa23de1e7e30a435
karlmaxV <karlmaxsonvicki@gmail.com>
2023-01-11 00:45:33 +0000

    all commits

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-
-
+# 8-load_from_json_file.py
+# Brennan D Baraban <375@holbertonschool.com>
+"""Defines a JSON file-reading function."""
 import json
 
 
 def load_from_json_file(filename):
-    with open(filename, "r", encoding="UTF-8") as f:
+    """Create a Python object from a JSON file."""
+    with open(filename) as f:
         return json.load(f)
